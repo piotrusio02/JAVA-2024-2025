bc2d47fab9a528d909fb7c6c3eab713fceb8b200
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-10-10 14:29:00 +0200

    Create zadanie_05.java

---
+++ Zadania z zajęć/zadanie_05.java
@@ -0,0 +1,15 @@
+/* tresc zadania */
+
+public class Main {
+    public static void main(String[] args) {
+        String slowo = "Zadanie";
+        System.out.println(odwrocNapis(slowo));
+    }
+    static String odwrocNapis(String napis){
+        StringBuilder odwrocony = new StringBuilder();
+        for (int i = napis.length()-1; i >= 0 ; i--) {
+            odwrocony.append(napis.charAt(i));
+        }
+        return odwrocony.toString();
+    }
+}
