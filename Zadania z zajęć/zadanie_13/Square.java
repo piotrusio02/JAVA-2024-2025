98046f0e39ef1db6fb3ee6f4f1c73a70482f4565
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-11-01 23:18:50 +0100

    Create Square.java

---
+++ Zadania z zajęć/zadanie_13/Square.java
@@ -0,0 +1,13 @@
+public class Square {
+    Integer side;
+
+    Square(Integer side){
+        this.side = side;
+    }
+    Square(){
+        this(1);
+    }
+    public void temp(){
+        System.out.println(side);
+    }
+}
