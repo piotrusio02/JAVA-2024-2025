40c757e5a696615ddef7968cb6ef1f702470b544
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-11-01 22:35:37 +0100

    Create Main.java

---
+++ Zadania z zajęć/zadanie_10/Main.java
@@ -0,0 +1,10 @@
+/* Stwórz dwie klasy: Parent i Child. Klasa Parent powinna mieć 
+  jedno pole protected. Spróbuj uzyskać dostęp do tego pola z klasy Child. */
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.test();
+
+    }
+}
