a6c7c5afc681c850c9a991bde84539052bba9f2d
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-11-01 22:24:16 +0100

    Update and rename Zadania z zajęć/zadanie_04.java to Zadania z zajęć/zadanie_04/Main.java

--- Zadania z zajęć/zadanie_04/Main.java
+++ Zadania z zajęć/zadanie_04/Main.java
@@ -14,17 +14,4 @@ public class Main {
     }
 }
 
-/*ListyTreningowe.java*/
-import java.util.ArrayList;
 
-public class ListyTrening {
-    public static int countZeros(ArrayList<Integer> liczby) {
-        int temp = 0;
-        for (int i = 0; i < liczby.size(); i++) {
-            if (liczby.get(i) == 0) {
-                temp += 1;
-            }
-        }
-        return temp;
-    }
-}
