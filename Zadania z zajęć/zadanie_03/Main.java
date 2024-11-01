d794f9fa20ce2c98919aa04d850a553f13ef8607
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-11-01 22:21:32 +0100

    Update and rename Zadania z zajęć/zadanie_03.java to Zadania z zajęć/zadanie_03/Main.java

--- Zadania z zajęć/zadanie_03/Main.java
+++ Zadania z zajęć/zadanie_03/Main.java
@@ -14,16 +14,3 @@ public class Main {
         System.out.println(listyTablicoweTrening.average(liczby));
     }
 }
-
-/* listyTablicoweTrening.java */
-import java.util.ArrayList;
-
-public class listyTablicoweTrening {
-    public static Double average(ArrayList<Double> lista) {
-        Double temp = 0.0;
-        for (int i = 0; i < lista.size(); i++) {
-            temp += lista.get(i);
-        }
-        return temp/lista.size();
-    }
-}
