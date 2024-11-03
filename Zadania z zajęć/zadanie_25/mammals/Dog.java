433abae0e5f05dce1daffb077d3642c08a84808e
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-11-03 01:20:07 +0100

    Create Dog.java

---
+++ Zadania z zajęć/zadanie_25/mammals/Dog.java
@@ -0,0 +1,11 @@
+package mammals;
+
+import animals.Animal;
+
+public class Dog extends Animal {
+    Dog(String species, int age){
+        super(species, age);
+    }
+    //age ma modyfikator private co oznacza ze jest dostępne tylko w klasie Animal
+
+}
