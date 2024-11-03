6257110c36d2bfcbc5d9511c93c6f20df1a628aa
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2024-11-03 01:47:08 +0100

    Create ExtendedCalculator.java

---
+++ Zadania z zajęć/zadanie_27/ExtendedCalculator.java
@@ -0,0 +1,7 @@
+public class ExtendedCalculator extends Calculator{
+
+    public static int add(int a, int b, int c){
+        return a+b+c;
+    }
+    /* Jest to przeciążenie. Nadpisując metodę nie możemy zmienić ilości argumentów. */
+}
