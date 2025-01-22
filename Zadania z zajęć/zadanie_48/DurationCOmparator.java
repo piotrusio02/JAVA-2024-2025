72d41082bf39ca7586244d81ad8d607c33bfd01f
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2025-01-22 22:37:21 +0100

    Create DurationCOmparator.java

---
+++ Zadania z zajęć/zadanie_48/DurationCOmparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class DurationComparator implements Comparator<Song> {
+    @Override
+    public int compare(Song o1, Song o2) {
+        return Integer.compare(o1.duration,o2.duration);
+    }
+}
