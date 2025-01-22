aeab4ccd78d64e8d09c6fe1bcc07862f5e5a4631
piotrusio02 <115213217+piotrusio02@users.noreply.github.com>
2025-01-22 22:24:36 +0100

    Create Person.java

---
+++ Zadania z zajęć/zadanie_47/Person.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Person {
+    String firstName;
+    String lastName;
+    LocalDate birthDate;
+
+    public Person(String firstName, String lastName, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
